5325bfbd79d59ce4d0bb9e0b58b18af0b3d801ed
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-30 01:50:21 +0100

    adding flashcard

---
+++ src/AppRouter.tsx
@@ -0,0 +1,33 @@
+import App from "./App.tsx";
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import "./index.scss";
+import { PageWelcome } from "./pages/PageWelcome.tsx";
+import { PageAbout } from "./pages/PageAbout.tsx";
+import { Page404 } from "./pages/Page404.tsx";
+import { PageFlashcards } from "./pages/PageFlashcards.tsx";
+
+export const router = createBrowserRouter([
+	{
+		path: "/",
+		errorElement: <Page404 />,
+		element: <App />,
+		children: [
+			{
+				path: "/welcome",
+				element: <PageWelcome />,
+			},
+			{
+				path: "flashcards",
+				element: <PageFlashcards />,
+			},
+			{
+				path: "about",
+				element: <PageAbout />,
+			},
+			{
+				path: "/",
+				element: <Navigate to="/welcome" replace />,
+			},
+		],
+	},
+]);
